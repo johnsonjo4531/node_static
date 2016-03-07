bef799b7a5466fa9da0e32704bf005bf1e61f662
John Johnson <johnsonjo4531@gmail.com>
2016-03-07 14:06:45 -0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -7,18 +7,18 @@ var gulp = require('gulp'),
     gWatch = require('gulp-watch');
 
 gulp.task('default', function() {
-    var trueBase = "public/assets/js";
-    return gWatch(trueBase + '/*-es6.js', function(obj){
+    var trueBase = "public/assets/js/";
+    return gWatch(trueBase + '/**/*-es6.js', function(obj){
         if (obj.event === 'change' || obj.event === 'add') {
             gulp.src(obj.path)
                 .pipe(plumber({
                     errorHandler: function (error) { /* elided */console.log(error); }
                 }))
-                .pipe(babel())
+                .pipe(babel({sourceMaps: true, compact: false}))
                 .pipe(rename(function (path) {
                     path.basename = path.basename.replace(/-es6$/, '');
                 }))
-                .pipe(gulp.dest(trueBase + obj.relative.replace(/(.*[\/\\]).*$/, '$1')))
+                .pipe(gulp.dest(trueBase + obj.relative.replace(/(.*[\/\\]+)?.*$/, '$1')))
                 .pipe(gprint(function(filePath){ return "File processed: " + filePath; }));
         }
     });
